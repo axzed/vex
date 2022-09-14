e13cf379cf37401c45b6dcfc7bb9e471b6e930d8
axzed <waxwc1491224820@gmail.com>
2022-09-14 16:52:34 +0800

    :sparkles: feat: add router level middlewares

--- vex.go
+++ vex.go
@@ -23,11 +23,12 @@ type MiddlewareFunc func(handleFunc HandleFunc) HandleFunc
 
 // Routing groups
 type routerGroup struct {
-	name             string                           // Router group's name
-	handleFuncMap    map[string]map[string]HandleFunc // Each routing group's handler's function
-	handlerMethodMap map[string][]string              // Support different request methods && its urls (store different request method type)
-	treeNode         *treeNode                        // prefix router match tree
-	middlewares      []MiddlewareFunc                 // middlewares function list
+	name               string                                 // Router group's name
+	handleFuncMap      map[string]map[string]HandleFunc       // Each routing group's handler's function
+	middlewaresFuncMap map[string]map[string][]MiddlewareFunc // Each routing group's middlewaresFunction's function
+	handlerMethodMap   map[string][]string                    // Support different request methods && its urls (store different request method type)
+	treeNode           *treeNode                              // prefix router match tree
+	middlewares        []MiddlewareFunc                       // middlewares function list
 }
 
 // Use function to add Middleware to the handleFunc
@@ -37,65 +38,80 @@ func (r *routerGroup) Use(middlewareFunc ...MiddlewareFunc) {
 }
 
 // methodHandle is the function when you need to executive the middleware in request
-func (r *routerGroup) methodHandle(handleFunc HandleFunc, ctx *Context) {
+func (r *routerGroup) methodHandle(name string, method string, handleFunc HandleFunc, ctx *Context) {
 	// if you have set the Middleware
 	// exec Middleware
+	// common level middleware
 	if r.middlewares != nil {
 		for _, middlewareFunc := range r.middlewares {
 			handleFunc = middlewareFunc(handleFunc)
 		}
 	}
-
+	// middlewareFuncs is router's middlewares function you set
+	middlewareFuncs := r.middlewaresFuncMap[name][method]
+	// exec the routerLevel middlewares
+	if middlewareFuncs != nil {
+		for _, middlewareFunc := range middlewareFuncs {
+			handleFunc = middlewareFunc(handleFunc)
+		}
+	}
 	// exec handleFunc you set in api
 	handleFunc(ctx)
 }
 
-// handle use this function to set the HandleFunc of the mapping url
-func (r *routerGroup) handle(name string, method string, handleFunc HandleFunc) {
+// handle use this function to set the HandleFunc and middlewares into the mapping url
+func (r *routerGroup) handle(name string, method string, handleFunc HandleFunc, middlewareFunc ...MiddlewareFunc) {
+	// use group's name to init handleFunc and middlewares list
 	_, ok := r.handleFuncMap[name]
+	// init the function of this group of routes
 	if !ok {
 		r.handleFuncMap[name] = make(map[string]HandleFunc)
+		r.middlewaresFuncMap[name] = make(map[string][]MiddlewareFunc)
 	}
 	_, ok = r.handleFuncMap[name][method]
 	if ok {
 		panic("With duplicate routes")
 	}
+	// add the handleFunc for the mapping route
 	r.handleFuncMap[name][method] = handleFunc
+	// add the middlewaresFunc for the mapping route
+	r.middlewaresFuncMap[name][method] = append(r.middlewaresFuncMap[name][method], middlewareFunc...)
+	// set the prefix tree's root node
 	r.treeNode.Put(name)
 }
 
 // Any Get Post Put Delete is restful api
 // Any is a method support any type of request to our router
-func (r *routerGroup) Any(name string, handleFunc HandleFunc) {
-	r.handle(name, ANY, handleFunc)
+func (r *routerGroup) Any(name string, handleFunc HandleFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, ANY, handleFunc, middlewareFunc...)
 }
 
-func (r *routerGroup) Get(name string, handleFunc HandleFunc) {
-	r.handle(name, http.MethodGet, handleFunc)
+func (r *routerGroup) Get(name string, handleFunc HandleFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodGet, handleFunc, middlewareFunc...)
 }
 
-func (r *routerGroup) Post(name string, handleFunc HandleFunc) {
-	r.handle(name, http.MethodPost, handleFunc)
+func (r *routerGroup) Post(name string, handleFunc HandleFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodPost, handleFunc, middlewareFunc...)
 }
 
-func (r *routerGroup) Delete(name string, handleFunc HandleFunc) {
-	r.handle(name, http.MethodDelete, handleFunc)
+func (r *routerGroup) Delete(name string, handleFunc HandleFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodDelete, handleFunc, middlewareFunc...)
 }
 
-func (r *routerGroup) Put(name string, handleFunc HandleFunc) {
-	r.handle(name, http.MethodPut, handleFunc)
+func (r *routerGroup) Put(name string, handleFunc HandleFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodPut, handleFunc, middlewareFunc...)
 }
 
-func (r *routerGroup) Patch(name string, handleFunc HandleFunc) {
-	r.handle(name, http.MethodPatch, handleFunc)
+func (r *routerGroup) Patch(name string, handleFunc HandleFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodPatch, handleFunc, middlewareFunc...)
 }
 
-func (r *routerGroup) Options(name string, handleFunc HandleFunc) {
-	r.handle(name, http.MethodOptions, handleFunc)
+func (r *routerGroup) Options(name string, handleFunc HandleFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodOptions, handleFunc, middlewareFunc...)
 }
 
-func (r *routerGroup) Head(name string, handleFunc HandleFunc) {
-	r.handle(name, http.MethodHead, handleFunc)
+func (r *routerGroup) Head(name string, handleFunc HandleFunc, middlewareFunc ...MiddlewareFunc) {
+	r.handle(name, http.MethodHead, handleFunc, middlewareFunc...)
 }
 
 // router defines a routerGroup's slice info
@@ -108,10 +124,11 @@ type router struct {
 // take the routerGroup to manipulate the url
 func (r *router) Group(name string) *routerGroup {
 	routerGroup := &routerGroup{
-		name:             name,
-		handleFuncMap:    make(map[string]map[string]HandleFunc),
-		handlerMethodMap: make(map[string][]string),
-		treeNode:         &treeNode{name: "/", children: make([]*treeNode, 0)},
+		name:               name,
+		handleFuncMap:      make(map[string]map[string]HandleFunc),
+		middlewaresFuncMap: make(map[string]map[string][]MiddlewareFunc),
+		handlerMethodMap:   make(map[string][]string),
+		treeNode:           &treeNode{name: "/", children: make([]*treeNode, 0)},
 	}
 	r.routerGroups = append(r.routerGroups, routerGroup)
 	return routerGroup
@@ -152,12 +169,12 @@ func (e *Engine) httpRequestHandle(w http.ResponseWriter, r *http.Request) {
 			}
 			handle, ok := group.handleFuncMap[node.routerName][ANY]
 			if ok {
-				group.methodHandle(handle, ctx)
+				group.methodHandle(node.routerName, ANY, handle, ctx)
 				return
 			}
 			handle, ok = group.handleFuncMap[node.routerName][method]
 			if ok {
-				group.methodHandle(handle, ctx)
+				group.methodHandle(node.routerName, method, handle, ctx)
 				return
 			}
 			// url matched but not in a correct method return 405
@@ -175,11 +192,6 @@ func (e *Engine) httpRequestHandle(w http.ResponseWriter, r *http.Request) {
 // It is a shortcut for http.ListenAndServe(addr, router)
 // Note: this method will block the calling goroutine indefinitely unless an error happens.
 func (e *Engine) Run() {
-	//for _, group := range e.routerGroups {
-	//	for key, value := range group.handleFuncMap {
-	//		http.HandleFunc(group.name+key, value)
-	//	}
-	//}
 	http.Handle("/", e)
 	err := http.ListenAndServe(":8111", nil)
 	if err != nil {
