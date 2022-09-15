85b8eb1be9ffa69f6a911a8f1f7e94cfbacab126
axzed <waxwc1491224820@gmail.com>
2022-09-15 15:43:25 +0800

    :sparkles: feat: add HTML template render

--- context.go
+++ context.go
@@ -4,7 +4,10 @@
 
 package vex
 
-import "net/http"
+import (
+	"html/template"
+	"net/http"
+)
 
 // Context is the most important part of gin. It allows us to pass variables between middleware,
 // manage the flow, validate the JSON of a request and render a JSON response for example
@@ -18,5 +21,29 @@ func (c Context) HTML(status int, html string) error {
 	c.W.Header().Set("Content-Type", "text/html; charset=utf-8")
 	c.W.WriteHeader(status)
 	_, err := c.W.Write([]byte(html))
+	return err
+}
+
+func (c Context) HTMLTemplate(name string, data any, filename ...string) error {
+	// Default status 200
+	c.W.Header().Set("Content-Type", "text/html; charset=utf-8")
+	t := template.New(name)
+	t, err := t.ParseFiles(filename...)
+	if err != nil {
+		return err
+	}
+	err = t.Execute(c.W, data)
+	return err
+}
+
+func (c Context) HTMLTemplateGlob(name string, data any, pattern string) error {
+	// Default status 200
+	c.W.Header().Set("Content-Type", "text/html; charset=utf-8")
+	t := template.New(name)
+	t, err := t.ParseGlob(pattern)
+	if err != nil {
+		return err
+	}
+	err = t.Execute(c.W, data)
 	return err
 }
