c7ae6758a7ed622eb153de24fe7b87f8b043da06
axzed <waxwc1491224820@gmail.com>
2022-09-22 14:54:22 +0800

    :sparkles: feat: add slice && array check

--- context.go
+++ context.go
@@ -342,29 +342,67 @@ func validateParam(obj any, decoder *json.Decoder) error {
 	}
 	elem := valueOf.Elem().Interface()
 	of := reflect.ValueOf(elem)
+	// judge the type of value
 	switch of.Kind() {
 	case reflect.Struct:
-		mapValue := make(map[string]interface{})
-		_ = decoder.Decode(&mapValue)
-		for i := 0; i < of.NumField(); i++ {
-			field := of.Type().Field(i)
-			name := field.Name
-			// get the json name by json tag and key
-			jsonName := field.Tag.Get("json")
-			if jsonName != "" {
-				name = jsonName
-			}
-			// self define the tag about vex framework
-			required := field.Tag.Get("vex")
-			value := mapValue[name]
+		return checkParam(of, obj, decoder)
+	case reflect.Slice, reflect.Array:
+		elem := of.Type().Elem()
+		if elem.Kind() == reflect.Struct {
+			return checkParamSlice(elem, obj, decoder)
+		}
+	default:
+		_ = decoder.Decode(obj)
+	}
+	return nil
+}
+
+// checkParamSlice is a method to handle the param in json and store in the slice or array
+func checkParamSlice(of reflect.Type, obj any, decoder *json.Decoder) error {
+	mapValue := make([]map[string]interface{}, 0)
+	_ = decoder.Decode(&mapValue)
+	for i := 0; i < of.NumField(); i++ {
+		field := of.Field(i)
+		name := field.Name
+		// get the json name by json tag and key
+		jsonName := field.Tag.Get("json")
+		if jsonName != "" {
+			name = jsonName
+		}
+		// self define the tag about vex framework
+		required := field.Tag.Get("vex")
+		for _, v := range mapValue {
+			value := v[name]
 			if value == nil && required == "required" {
 				return errors.New(fmt.Sprintf("filed [%s] is not exist, because [%s] is required", jsonName, jsonName))
 			}
 		}
-		b, _ := json.Marshal(mapValue)
-		_ = json.Unmarshal(b, obj)
-	default:
-		_ = decoder.Decode(obj)
 	}
+	b, _ := json.Marshal(mapValue)
+	_ = json.Unmarshal(b, obj)
+	return nil
+}
+
+// checkParam is a method handle the param by tag and check the validation of json param in request body
+func checkParam(of reflect.Value, obj any, decoder *json.Decoder) error {
+	mapValue := make(map[string]interface{})
+	_ = decoder.Decode(&mapValue)
+	for i := 0; i < of.NumField(); i++ {
+		field := of.Type().Field(i)
+		name := field.Name
+		// get the json name by json tag and key
+		jsonName := field.Tag.Get("json")
+		if jsonName != "" {
+			name = jsonName
+		}
+		// self define the tag about vex framework
+		required := field.Tag.Get("vex")
+		value := mapValue[name]
+		if value == nil && required == "required" {
+			return errors.New(fmt.Sprintf("filed [%s] is not exist, because [%s] is required", jsonName, jsonName))
+		}
+	}
+	b, _ := json.Marshal(mapValue)
+	_ = json.Unmarshal(b, obj)
 	return nil
 }
